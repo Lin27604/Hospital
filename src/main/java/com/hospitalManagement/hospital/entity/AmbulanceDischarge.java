3154dfd290140024baedf3ec9110f4ff17e7c3f6
Lin27604 <dlin@student.fitchburgstate.edu>
2022-07-03 14:10:00 -0400

    first commit

---
+++ src/main/java/com/hospitalManagement/hospital/entity/AmbulanceDischarge.java
@@ -0,0 +1,10 @@
+package com.hospitalManagement.hospital.entity;
+
+public class AmbulanceDischarge {
+	private Long id;
+	private String startTime;
+	private String finishTime;
+	private Ambulance ambulance;
+	private Emergency emergency;
+
+}
---
+++ src/main/java/com/hospitalManagement/hospital/entity/Staff.java
@@ -0,0 +1,13 @@
+package com.hospitalManagement.hospital.entity;
+
+public class Staff {
+	private Long id;
+	private String name;
+	private String dob;
+	private String gender;
+	private String position;
+	private Address address;
+	
+	
+
+}
