c378ee917cac2731954fd8714ab188fc32822a98
xchen200 <xchen200@hawk.iit.edu>
2022-07-07 10:50:15 -0500

    add doctor and patient

---
+++ Hospital-master/src/main/java/com/hospitalManagement/hospital/entity/Doctor.java
@@ -0,0 +1,69 @@
+package main.java.com.hospitalManagement.hospital.entity;
+
+import java.util.Date;
+
+public class Doctor {
+    private Long doctorId;
+    private Long staffId;
+    private Long departmentId;
+    private String department;
+    private Long registeredId;
+    private String type;
+    private Date date;
+
+    public Long getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(Long doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public Long getStaffId() {
+        return staffId;
+    }
+
+    public void setStaffId(Long staffId) {
+        this.staffId = staffId;
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Long getRegisteredId() {
+        return registeredId;
+    }
+
+    public void setRegisteredId(Long registeredId) {
+        this.registeredId = registeredId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
---
+++ Hospital-master/src/main/java/com/hospitalManagement/hospital/entity/Patient.java
@@ -0,0 +1,31 @@
+package main.java.com.hospitalManagement.hospital.entity;
+
+public class Patient {
+    private Long patientId;
+    private Long registerId;
+    private Long recordId;
+
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public Long getRegisterId() {
+        return registerId;
+    }
+
+    public void setRegisterId(Long registerId) {
+        this.registerId = registerId;
+    }
+
+    public Long getRecordId() {
+        return recordId;
+    }
+
+    public void setRecordId(Long recordId) {
+        this.recordId = recordId;
+    }
+}
